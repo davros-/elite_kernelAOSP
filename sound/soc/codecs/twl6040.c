b546b95199b5c4065686689bde124afe4d17ceef
davros <davros@droidconcepts.com>
2013-03-06 03:02:35 -0500

    fix never booting with headphones plugged in

--- sound/soc/codecs/twl6040.c
+++ sound/soc/codecs/twl6040.c
@@ -956,9 +956,13 @@ void soundcontrol_reportjack(int jack_type)
 {
     if (jack_type == 0) {
 	headset_plugged = false;
+	
+    if (snd_codec != NULL)
 	headset_power_mode(snd_codec, 1);
     } else { 
 	headset_plugged = true;
+	
+    if (snd_codec != NULL && snd_data != NULL)
 	headset_power_mode(snd_codec, snd_data->headset_mode);
     }
 
